ea522ffdbe497b260e545bcc04aef44f158ad0ae
Renato Xavier <rnatu91@gmail.com>
2023-06-04 16:53:33 -0300

    feat(PartnerGroup): add all style and variant style

---
+++ src/components/PartnerGroup/index.tsx
@@ -0,0 +1,31 @@
+import style from './style.module.scss';
+
+interface PartnerGroupProps {
+  variant?: boolean;
+}
+
+export function PartnerGroup({ variant = false }: PartnerGroupProps) {
+  return (
+    <section
+      className={`${style.partnerGroupContainer} ${variant && style.variant}`}
+    >
+      <div>
+        <div className={style.partnerGroupMask} />
+        <div className={style.partnerGroupContent}>
+          <h1>Parceiros</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur</p>
+          <button>Confira</button>
+        </div>
+      </div>
+
+      <div>
+        <div className={style.partnerGroupMask} />
+        <div className={style.partnerGroupContent}>
+          <h1>Parceiros</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur</p>
+          <button>Confira</button>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,20 +3,31 @@ import { Header } from '../../components/Header';
 import { Departments } from '../../components/Departments';
 import { RelatedProducts } from '../../components/RelatedProducts';
 import { RelatedCategories } from '../../components/RelatedCategories';
+import { PartnerGroup } from '../../components/PartnerGroup';
+import style from './styles.module.scss';
 
 export function Home() {
   return (
     <>
       <Header />
+
       <Banner />
+
       <Departments />
-      <RelatedProducts>
-        <RelatedCategories />
-      </RelatedProducts>
-      <RelatedProducts>
-        {/* <RelatedCategories /> */}
-        <a href="/">Ver todos</a>
-      </RelatedProducts>
+
+      <main className={style.mainContainer}>
+        <RelatedProducts>
+          <RelatedCategories />
+        </RelatedProducts>
+
+        <PartnerGroup />
+
+        <RelatedProducts>
+          <a href="/">Ver todos</a>
+        </RelatedProducts>
+
+        <PartnerGroup variant />
+      </main>
     </>
   );
 }
